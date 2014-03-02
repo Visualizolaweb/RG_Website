1e53f37369c6ef6d2a26db82f0ce586cc1b27489
claom <claom397@hotmail.com>
2014-03-02 14:52:08 -0500

    add file

--- _includes/menu.php
+++ _includes/menu.php
@@ -2,8 +2,9 @@
 	<h1>RG Group - Hotels & Inmobiliaria</h1>
 	<ul>
 		<li><a href="home.php" title="INICIO">INICIO</a></li>
-		<li><a href="hotels.php" title="HOTELES AQUA">HOTELES AQUA</a></li>
 		<li><a href="about_us.php" title="SOBRE NOSOTROS">SOBRE NOSOTROS</a></li>
+		<li><a href="hotels.php" title="HOTELES AQUA">HOTELES AQUA</a></li>
+		<li><a href="discover.php" title="DESCUBRE">DESCUBRE</a></li>
 		<li><a href="booking.php" title="RESERVAS">RESERVAS</li>
 		<li><a href="contact.php" title="CONTACTO">CONTACTO</a></li>
 	</ul>
---
+++ discover.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>RG Group Hotels</title>
+</head>
+<body>
+	<section>
+		<header><h1>Discover</h1></header>
+		<?php include("_includes/menu.php");?>
+
+		<article>
+			<h2>Llegue a Panama y descubra otros mundos sin salir de la misma...</h2>
+¡Le damos la bienvenida a los  hoteles y del grupo RG PANAMA! Todos ellos han sido creados pensando en cada una de las necesidades de nuestros huéspedes. Cada uno, a su manera, le invita a gozar de un ambiente único. ¡Le damos la bienvenida al corazón de la originalidad y la buena convivencia!</p>		
+		</article>
+	</section>
+</body>
+</html>
