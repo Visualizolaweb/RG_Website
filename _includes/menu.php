937507c7c4c36b294055e54b68e48d37ae1c7189
claom <claom397@hotmail.com>
2014-03-02 13:56:59 -0500

    Adicion menu hoteles y creacion de pagina hoteles

--- _includes/menu.php
+++ _includes/menu.php
@@ -1,6 +1,7 @@
 <nav>
 	<ul>
 		<li><a href="home.php" title="INICIO">INICIO</a></li>
+		<li><a href="hotels.php" title="HOTELES AQUA">HOTELES AQUA</a></li>
 		<li><a href="about_us.php" title="SOBRE NOSOTROS">SOBRE NOSOTROS</a></li>
 		<li><a href="booking.php" title="RESERVAS">RESERVAS</li>
 		<li><a href="contact.php" title="CONTACTO">CONTACTO</a></li>
